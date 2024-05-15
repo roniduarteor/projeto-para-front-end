c2d336a5dba31fa0644af52dfcabbfbb2ae224ac
Ronaldo Duarte <138784850+roniduarteor@users.noreply.github.com>
2024-05-15 12:09:09 -0300

    Add files via upload

--- styled-components/perfum-card/src/Styles/App.js
+++ styled-components/perfum-card/src/Styles/App.js
@@ -8,12 +8,24 @@ export const Container = styled.section`
   height: 650px;
   background-color: white;
   border-radius: 10px;
+
+   @media screen and (min-width: 800px){
+    width: 850px;
+    display: flex;
+    flex-direction: row;
+  } 
 `
 
 export const Image = styled.img`
-  width: 375px;
+  width: 100%;
+  height: 100%;
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
+
+  @media screen and (min-width: 800px){
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 10px;
+  } 
 `
 
 export const InformationContainer = styled.section`
@@ -22,6 +34,12 @@ export const InformationContainer = styled.section`
   flex-direction: column;
   justify-content: space-between;
   height: 100%;
+
+  @media screen and (min-width: 800px){
+    width: 100%;
+    flex-direction: column;
+    justify-content: space-around;
+  } 
 `
 
 export const ProductType = styled.p`
@@ -30,12 +48,21 @@ export const ProductType = styled.p`
   font-size: 13px;
   font-weight: 400;
   color: grey;
+
+  @media screen and (min-width: 800px){
+    font-size: 18px;
+  } 
 `
 
 export const Title = styled.h1`
   font-family: Fraunces;
   font-size: 35px;
   line-height: 35px;
+
+  @media screen and (min-width: 800px){
+    font-size: 55px;
+    line-height: 60px;
+  } 
 `
 
 export const Text = styled.p`
@@ -44,6 +71,11 @@ export const Text = styled.p`
   color: grey;
   line-height: 28px;
   font-weight: light;
+
+  @media screen and (min-width: 800px){
+    font-size: 20px;
+    line-height: 34px;
+  } 
 `
 
 export const Value = styled.span`
@@ -57,6 +89,10 @@ export const Promotion = styled.h2`
   font-size: 30px;
   color: hsl(158, 36%, 37%);
   margin-right: 20px;
+
+  @media screen and (min-width: 800px){
+    font-size: 40px;
+  } 
 `
 
 export const Price = styled.p`
@@ -79,6 +115,14 @@ export const Button = styled.button`
   gap: 10px;
 
   cursor: pointer;
+
+  &:hover{
+    background-color: hsl(180, 36%, 37%);
+  }
+
+  @media screen and (min-width: 800px){
+    height: 65px;
+  }
 `
 
 export const TextButton = styled.p`
@@ -86,4 +130,8 @@ export const TextButton = styled.p`
   font-family: Montserrat;
   font-weight: 700;
   font-size: 15px;
+
+  @media screen and (min-width: 800px){
+    font-size: 18px;
+  } 
 `
